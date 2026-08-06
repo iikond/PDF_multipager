a119d6a84ce7492ea28a7d4dbdc6b932a8898767
iikond <idkondrahin@yandex.ru>
2026-08-06 21:57:04 +0300

    initial commit

---
+++ PDF_multipager.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from PIL import Image
+
+output_dir = Path("pdf")
+output_dir.mkdir(exist_ok=True)
+
+files = {}
+
+for file in Path(".").glob("*.bmp"):
+    doc, page = map(int, file.stem.split("_"))
+    files.setdefault(doc, []).append((page, file))
+
+for doc, pages in files.items():
+    pages.sort()
+
+    images = []
+
+    for _, path in pages:
+        img = Image.open(path).convert("RGB")
+        images.append(img)
+    
+    images[0].save(
+        output_dir / f"{doc}.pdf",
+        save_all=True,
+        append_images=images[1:]
+    )
+    
+print("Готово.")
